2e17fe799c7c8c8255f63b45fb5924c8a6ec9e63
Rishabh Rathore <rishabh.cd231081@global.org.in>
2025-08-23 15:49:36 +0530

    third commit

---
+++ type-casting.py
@@ -0,0 +1,19 @@
+a = 123
+b = 3.32
+print(type(a))
+print(type(b))
+c=a+b
+print(c)
+print(type(c))  #Implicit type-conversion (Done by python)
+
+d = "123"
+print(type(d))
+d = int(d)
+print("After conversion",type(d)) #Explicit type-conversion (Done by user)
+
+
+
+
+
+
+
