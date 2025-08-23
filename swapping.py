88ad332709258a0732651f087389bc9a03166c08
Rishabh Rathore <rishabh.cd231081@global.org.in>
2025-08-23 16:14:51 +0530

    fourth commit

---
+++ swapping.py
@@ -0,0 +1,13 @@
+a = 10    #Method-1
+b = 20
+print("Before swapping ",a,b)
+temp = a
+a = b
+b = temp
+print("After swapping ",a,b)
+
+c = 4 #Method-2
+d = 5
+print("Before swapping ",c,d)
+c,d = d,c
+print("After swapping ",c,d)
