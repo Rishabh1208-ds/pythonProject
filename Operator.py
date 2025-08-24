99e3eca538e0fda3abeaf1a1ba90550e2c42f530
Rishabh Rathore <rishabh.cd231081@global.org.in>
2025-08-24 18:54:54 +0530

    fivth commit

---
+++ Operator.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+print(a==b or b>a)     #lOGICAL OPERATOR
+print(a<b and b<a)
+print(not(a==b or b>a))
+
+print(a is not b)      #IDENTITY OPERATOR
+print(a is b)
+
+print(a ^ b)           #XOR OPERATOR
+print(0 ^ 0)
+print(0 ^ 1)
